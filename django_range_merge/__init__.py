d24e62b4afaa7884cdc3e8088a698a5a52a2c047
Jack Linke <jacklinke@gmail.com>
2022-10-09 02:04:53 -0400

    Bump version

--- django_range_merge/__init__.py
+++ django_range_merge/__init__.py
@@ -2,6 +2,6 @@
 Enables the range_merge Aggregate for Django on Postgres
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 default_app_config = "django_range_merge.apps.DjangoRangeMergeConfig"  # pylint: disable=invalid-name
